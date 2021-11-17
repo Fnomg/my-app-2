094fe90d7143e96038c72eff9425ac273001a919
Anna <annie_z@bk.ru>
2021-11-17 06:18:20 +0300

    фикс

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import {Message} from "./components/Messages";
 
+const messages = [
+  {
+    id: 0,
+    name: 'Ann',
+    text: 'Hi'
+  },
+  {
+    id: 1,
+    name: 'Ben',
+    text: 'Hello'
+  }
+]
+
 function App() {
-  const messages = [
-    {
-      id: 0,
-      name: 'Ann',
-      text: 'Hi'
-    },
-    {
-      id: 1,
-      name: 'Ben',
-      text: 'Hello'
-    }
-  ]
+
   return (
     <div className="App">
       <Message messages={messages}/>
