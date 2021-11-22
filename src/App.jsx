58b756012ea0ce31479b1f5520bd5f76d69eb256
Anna <annie_z@bk.ru>
2021-11-22 07:01:45 +0300

    lesson-3
    material-ui
    автофокус
    массив чатов

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
-import {Message} from "./components/Messages";
+import * as React from 'react';
+import {Sidebar} from "./components/Sidebar/Sidebar";
+import {MainSection} from "./components/MainSection/MainSection";
 
-function App() {
-
-  const [messageList, setMessageList] = useState([]);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    let timer;
-    if (messageList.length !== 0 && messageList[messageList.length - 1]?.author !== 'Good') {
-      timer = setTimeout(() => sendMessage('Good'), 1500);
-    }
-    return () => clearTimeout(timer);
-  },[messageList, setMessage]);
-
-  const handleChangeMessageText = (e) => {
-    setMessage(e.target.value)
-  }
-
-  function sendMessage (author){
-    let newMessage = {
-      id: uuidv4(),
-      author: author,
-      text: message !== '' ? message : 'Приветики'
-    }
-    setMessage('')
-    setMessageList([...messageList, newMessage]);
-  }
+const App = () => {
 
   return (
-    <div className="App">
-      <Message messages={messageList}/>
-      <input type="text" onChange={handleChangeMessageText} value={message}/>
-      <button onClick={() => sendMessage('Ann')}>Отправить</button>
-    </div>
-  );
+    <>
+      <div className="content">
+        <Sidebar />
+        <MainSection />
+      </div>
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useRef, useState} from "react";
+import {Messages} from "../Messages/Messages";
+import {Button, TextField} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import {v4 as uuidv4} from "uuid";
+
+export const MainSection = () => {
+
+  const [messageList, setMessageList] = useState([]);
+  const [textMessage, setTextMessage] = useState('');
+  const ref = useRef(null);
+
+  useEffect(() => {
+    let timer;
+    if (messageList.length !== 0 && messageList[messageList.length - 1]?.author !== 'Good') {
+      timer = setTimeout(() => sendNewMessage(), 1500);
+    }
+    return () => clearTimeout(timer);
+  },[messageList]);
+
+  useEffect(()=>{
+    ref.current?.focus();
+  })
+
+
+  function sendNewMessage (author = 'Good'){
+    let newMessage = {
+      id: uuidv4(),
+      author: author,
+      text: textMessage !== '' ? textMessage : 'Заглушка для робота'
+    }
+    setTextMessage('')
+    setMessageList([...messageList, newMessage]);
+    console.log(newMessage)
+  }
+
+  const handleTextChange = (e) => {
+    setTextMessage(e.target.value)
+  }
+
+  console.log(messageList)
+
+
+  return (
+    <div className="box">
+      <Messages messageList={messageList}/>
+      <TextField
+        id="outlined-multiline-flexible"
+        multiline
+        maxRows={4}
+        value={textMessage}
+        onChange={handleTextChange}
+        autoFocus
+        inputRef={ref}
+      />
+      <Button onClick={()=>sendNewMessage('Ann')} variant="contained" endIcon={<SendIcon />} >
+        Send
+      </Button>
+    </div>
+  )
+}
