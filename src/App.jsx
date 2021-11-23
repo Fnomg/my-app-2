075f56a76c3a4ef306873ae3e4e358d094a790cd
Anna <annie_z@bk.ru>
2021-11-23 06:25:03 +0300

    lesson-4
    роутинг
    новые страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import './App.css';
 import * as React from 'react';
-import {ChatList} from "./components/ChatList/ChatList";
-import {MessageList} from "./components/MessageList/MessageList";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {Main} from "./pages/Main";
+import {Chats} from "./pages/Chats";
+import {Chat} from "./pages/Chat";
+import {Friends} from "./pages/Friends";
+import {Feed} from "./pages/Feed";
+import {PageNotFound} from "./pages/PageNotFound";
+import {
+  CHATS_ROUTE, FEED_ROUTE,
+  FRIENDS_ROUTE,
+  MAIN_ROUTE, NOT_FOUND_ROUTE, CHATS_ID_ROUTE
+} from './constants/routing';
 
 const App = () => {
 
   return (
-    <>
+    <Router>
+      <nav>
+        <Link to={MAIN_ROUTE}>Моя страница</Link>
+        <Link to={CHATS_ROUTE}>Мессенеджер</Link>
+        <Link to={FRIENDS_ROUTE}>Друзья</Link>
+        <Link to={FEED_ROUTE}>Новости</Link>
+      </nav>
       <div className="content">
-        <ChatList />
-        <MessageList />
+        <Routes>
+          <Route path={MAIN_ROUTE} element={<Main />}/>
+          <Route path={CHATS_ROUTE} element={<Chats />}>
+            <Route path={CHATS_ID_ROUTE} element={<Chat />}/>
+          </Route>
+          <Route path={FRIENDS_ROUTE} element={<Friends />}/>
+          <Route path={FEED_ROUTE} element={<Feed />}/>
+          <Route path={NOT_FOUND_ROUTE} element={<PageNotFound />}>Page not found</Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
       </div>
-    </>
+    </Router>
 
   )
 }
---
+++ src/pages/Chats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {ChatList} from "../components/ChatList/ChatList";
+import {MessageList} from "../components/MessageList/MessageList";
+
+export const Chats = () => {
+  return (
+    <>
+      <ChatList />
+      <MessageList />
+    </>
+
+  )
+}
